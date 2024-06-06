9b0c6c34758528eeaddd5320a5186b0c385c1f16
andrea-bertaglia <andreabertaglia@gmail.com>
2024-06-06 17:10:59 +0200

    feat: routing tests

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -4,13 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>LARAVEL PRIMI PASSI</title>
+    <title>TEST</title>
 </head>
 <body>
-    <h1>Hello World!</h1>
+    <h1>Questa è la pagina di test</h1>
 
     <a href="/">HOME</a>
     <a href="test">TEST</a>
+
 
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name("/");
+
+Route::get('/test', function () {
+    return view('test');
+})->name("test");
